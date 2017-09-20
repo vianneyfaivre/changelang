ee09c59edc85f426e798711182a74fd4e3a05de9
Vianney FAIVRE <vianney.faivre@gmail.com>
2017-09-20 17:28:56 -0400

    initial commit

---
+++ src/main/java/re/vianneyfaiv/changelang/ChangelangApplication.java
@@ -0,0 +1,12 @@
+package re.vianneyfaiv.changelang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangelangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangelangApplication.class, args);
+	}
+}
